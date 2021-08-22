5a9b0ac820d6082cf7165f1f577b142e282af228
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-08-22 14:00:55 -0400

    Agregamos el formulario de pago

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Payment</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <div class="card-body">
+                        <form action="#" method="post" id="paymentForm">
+                            <div class="row">
+                                <div class="col">
+                                    <label for="">How much you want to pay?</label>
+                                    <input type="number" min="5" step="0.01" class="form-control" name="values"
+                                           value="{{mt_rand(500,100000)/100}}">
+                                    <small class="form-text text-muted">
+                                        Use values with up to tow decimal positions,using dot "."
+                                    </small>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col text-center mt-2">
+                                    <button class="btn btn-primary" type="submit">Pay</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
