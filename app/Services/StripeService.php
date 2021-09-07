1070a6fbc0c19477386f17f37194e13f9b9f3870
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-09-07 16:57:04 -0400

    Creamos el servicio para Stripe

---
+++ app/Services/StripeService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Services;
+
+use App\Traits\ConsumesExtenalServices;
+use Illuminate\Http\Request;
+
+class StripeService
+{
+    use ConsumesExtenalServices;
+
+    protected $baseUri;
+    protected $key;
+    protected $secret;
+
+    public function __construct()
+    {
+        $this->baseUri = config('services.stripe.base_uri');
+        $this->key = config('services.stripe.key');
+        $this->secret = config('services.stripe.secret');
+    }
+
+    public function resolveAuthorization(&$queryParams, &$formParams, &$headers)
+    {
+        $headers['Authorization'] = $this->resolveAccessToken();
+    }
+
+    public function decodeResponse($response)
+    {
+        return json_decode($response);
+    }
+
+    public function resolveAccessToken()
+    {
+
+    }
+
+    public function handlePayment(Request $request)
+    {
+
+    }
+
+    public function handleApproval()
+    {
+
+    }
+
+    public function resolveFactor($currency)
+    {
+        $zeroDecimalCurrencies = ['JPY'];
+        if (in_array(strtoupper($currency), $zeroDecimalCurrencies)) {
+            return 1;
+        }
+        return 100;
+    }
+}
